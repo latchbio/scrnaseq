6763abce839a2a1f38e9c61f78cd3922d08dd619
AidanAbd <aidan@latch.bio>
2022-07-19 22:42:05 -0700

    fix: dur

--- wf/__init__.py
+++ wf/__init__.py
@@ -828,7 +828,7 @@ def h5ad(
     gene_types = []
     gene_long_name = []
     for gid in gene_names:
-        query_res: Dict[str, str] = gene_info_by_input.get(gid, Dict())
+        query_res: Dict[str, str] = gene_info_by_input.get(gid, dict())
         gene_symbols.append(query_res.get("symbol", None))
         gene_types.append(query_res.get("type_of_gene", None))
         gene_long_name.append(query_res.get("name", None))
